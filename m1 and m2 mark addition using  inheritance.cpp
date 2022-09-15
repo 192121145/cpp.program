ec84b0ef21b358c508fe3469bc822e28f41a6de5
192121145 <113018423+192121145@users.noreply.github.com>
2022-09-15 14:54:42 +0530

    Add files via upload

---
+++ m1 and m2 mark addition using  inheritance.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class student
+{
+	int rollno;
+	public:
+		void student1 ()
+	    {
+		    cout<<"enter a rollno:";
+		    cin>>rollno;
+	    }
+};
+class test:public student
+{
+	
+	public:
+		int m1,m2;
+		void testmark()
+		{
+			cout<<"enter a mark m1 amd m2:";
+			cin>>m1>>m2;
+		
+		}
+};
+class result:public test
+{
+	public:
+		 void totalmark()
+		 {
+		 	cout<<"totalmark of m1 and m2="<<m1+m2;
+		 }	
+};
+int main()
+{
+	 result c;
+	c.student1();
+	c.testmark();
+	c.totalmark();
+}
